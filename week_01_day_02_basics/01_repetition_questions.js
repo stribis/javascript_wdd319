15930e737dee22db3c9998ca0472549e1ea93b28
Martin Hutchings <martin.hutchings@sae.edu>
2019-07-23 14:58:58 +0200

    added day 2

---
+++ week_01_day_02_basics/01_repetition_questions.js
@@ -0,0 +1,44 @@
+/* Follow the guidelines in the comments */
+/* Some code is incorrect and needs fixing */
+
+/* 1. Define a string called name and make it equal to your own name  */
+
+var name = 'Martin'
+
+/* 2. Define a number called age and make it equal to your own age */
+
+var number = 27
+
+/* 3. Define a string called hobby and make it equal to your favorite hobby */
+
+var hobby = 'Baseball'
+
+/* 4. Fix this console log so that it correctly concatenates your string */
+
+
+console.log('My name is ' + name +', I am ' + number+  ' years old and love '+ hobby + '!')
+
+
+/* 5. This console.log should be printing out 25 */
+
+var numb = 25;
+
+if( numb === 25) {
+  console.log(numb);
+}
+
+
+/* 6. Something is wrong with this shopping list */
+
+
+var shoppingList = ['apples', 'bananas', 'pears', 'oranges'];
+
+console.log(shoppingList);
+
+
+/* 7. Now that you have fixed the shopping list, lets print out each individual item */
+
+
+for(var i = 0; i < shoppingList.length(); i+1){
+  console.log(shoppingList[i])
+}
